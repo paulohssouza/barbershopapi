bf48d898474a89f6ec9b91ebb237aec3eb6a3937
Paulo Henrique <paulohss.souza@gmail.com>
2024-03-13 09:15:43 -0300

    Inicio do projeto barbearia api

---
+++ src/main/java/br/com/paulohss/barbershopapi/BarbershopapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.paulohss.barbershopapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarbershopapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BarbershopapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/paulohss/barbershopapi/BarbershopapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.paulohss.barbershopapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarbershopapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
